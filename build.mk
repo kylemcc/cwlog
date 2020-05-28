cb649a4320e6251185842b3fc6a33b09130d2ea2
Kyle McCullough <kylemcc@gmail.com>
2020-05-28 17:57:02 -0500

    makefile: update help

--- build.mk
+++ build.mk
@@ -76,4 +76,4 @@ check: test fmt lint staticcheck vet ## Runs test, fmt, lint, staticcheck, and v
 
 .PHONY: help
 help:
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort |  awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | cut -d ':' -f2- | sort |  awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
